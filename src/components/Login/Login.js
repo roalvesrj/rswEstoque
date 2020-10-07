c87642a70554b853b601fcda34ab3c240aeb8046
Rômulo Alves <romuloalves.rj@gmail.com>
2020-10-07 19:57:01 -0300

    Correção responsividade Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,107 +1,107 @@
-import React, {useState, Component} from 'react';
+import React, { useState, Component } from 'react';
 import {
-  View,
-  Text,
-  Image,
-  TouchableHighlight,
-  Alert,
-  Modal,
-  TextInput,
-  ScrollView,
+    View,
+    Text,
+    Image,
+    TouchableHighlight,
+    Alert,
+    Modal,
+    TextInput,
+    ScrollView,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Styles from './Style';
 
-const Login = ({navigation}) => {
-  const [modalVisible, setModalVisible] = useState(false);
+const Login = ({ navigation }) => {
+    const [modalVisible, setModalVisible] = useState(false);
 
-  return (
-    <>
-      <View style={Styles.container}>
-        <Image
-          style={Styles.image}
-          source={require('./../../../assets/images/deal-logo.png')}
-        />
-        <Text style={Styles.destaque}>
-          Giro de estoque, Inventário, Curva ABC e Gestão eficiente
-        </Text>
-        <Text style={Styles.descricao}>
-          Faça login para equilibrar suas compras, armazenagem e entregas
-        </Text>
-        <TouchableHighlight
-          style={Styles.button}
-          underlayColor="#feec90"
-          onPress={() => {
-            setModalVisible(true);
-          }}>
-          <View style={{flexDirection: 'row'}}>
-            <Icon name="login" style={Styles.icon} />
-            <Text style={Styles.buttonTexto}>FAZER LOGIN</Text>
-          </View>
-        </TouchableHighlight>
-        <Text style={Styles.link}>ESQUECI MINHA SENHA</Text>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            setModalVisible(!modalVisible);
-          }}>
-          <View style={Styles.centeredView}>
-            <View style={Styles.modalView}>
-              <View
-                style={{alignContent: 'space-between', flexDirection: 'row'}}>
-                <View>
-                  <Text>Faça o Login</Text>
-                </View>
-                <View>
-                  <TouchableHighlight
-                    style={Styles.openButton}
+    return (
+        <ScrollView style={Styles.scroll}>
+            <View style={Styles.container}>
+                <Image
+                    style={Styles.image}
+                    source={require('./../../../assets/images/deal-logo.png')}
+                />
+                <Text style={Styles.destaque}>
+                    Giro de estoque, Inventário, Curva ABC e Gestão eficiente
+                </Text>
+                <Text style={Styles.descricao}>
+                    Faça login para equilibrar suas compras, armazenagem e entregas
+                </Text>
+                <TouchableHighlight
+                    style={Styles.button}
+                    underlayColor="#feec90"
                     onPress={() => {
-                      setModalVisible(!modalVisible);
+                        setModalVisible(true);
                     }}>
-                    <Icon name="close" style={Styles.icon} />
-                  </TouchableHighlight>
-                </View>
-              </View>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Icon name="login" style={Styles.icon} />
+                        <Text style={Styles.buttonTexto}>FAZER LOGIN</Text>
+                    </View>
+                </TouchableHighlight>
+                <Text style={Styles.link}>ESQUECI MINHA SENHA</Text>
 
-              <TextInput
-                style={{
-                  height: 40,
-                  marginBottom: 10,
-                  borderColor: 'gray',
-                  borderWidth: 1,
-                }}
-              />
-              <TextInput
-                style={{
-                  height: 40,
-                  marginBottom: 10,
-                  borderColor: 'gray',
-                  borderWidth: 1,
-                }}
-                secureTextEntry={true}
-              />
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        setModalVisible(!modalVisible);
+                    }}>
+                    <View style={Styles.centeredView}>
+                        <View style={Styles.modalView}>
+                            <View
+                                style={{ alignContent: 'space-between', flexDirection: 'row' }}>
+                                <View>
+                                    <Text>Faça o Login</Text>
+                                </View>
+                                <View>
+                                    <TouchableHighlight
+                                        style={Styles.openButton}
+                                        onPress={() => {
+                                            setModalVisible(!modalVisible);
+                                        }}>
+                                        <Icon name="close" style={Styles.icon} />
+                                    </TouchableHighlight>
+                                </View>
+                            </View>
 
-              <TouchableHighlight
-                style={Styles.button}
-                underlayColor="#feec90"
-                onPress={() =>
-                  navigation.navigate('Home', setModalVisible(!modalVisible))
-                }>
-                <View style={{flexDirection: 'row'}}>
-                  <Icon name="login" style={Styles.icon} />
-                  <Text style={Styles.buttonTexto}>ACESSAR</Text>
-                </View>
-              </TouchableHighlight>
+                            <TextInput
+                                style={{
+                                    height: 40,
+                                    marginBottom: 10,
+                                    borderColor: 'gray',
+                                    borderWidth: 1,
+                                }}
+                            />
+                            <TextInput
+                                style={{
+                                    height: 40,
+                                    marginBottom: 10,
+                                    borderColor: 'gray',
+                                    borderWidth: 1,
+                                }}
+                                secureTextEntry={true}
+                            />
+
+                            <TouchableHighlight
+                                style={Styles.button}
+                                underlayColor="#feec90"
+                                onPress={() =>
+                                    navigation.navigate('Home', setModalVisible(!modalVisible))
+                                }>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Icon name="login" style={Styles.icon} />
+                                    <Text style={Styles.buttonTexto}>ACESSAR</Text>
+                                </View>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </Modal>
             </View>
-          </View>
-        </Modal>
-      </View>
-    </>
-  );
+        </ScrollView>
+    );
 };
 
 export default Login;
