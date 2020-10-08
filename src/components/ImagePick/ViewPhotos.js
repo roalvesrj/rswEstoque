f2fe4dc2f9581ec2b97f97c9d369cb10229829f3
Diego <diego.canedo86@gmail.com>
2020-10-08 15:35:54 -0300

    Adicionando scrollview na Home

--- src/components/ImagePick/ViewPhotos.js
+++ src/components/ImagePick/ViewPhotos.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const ViewPhotos = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  return <></>;
+};
 
-export default ViewPhotos
+export default ViewPhotos;
