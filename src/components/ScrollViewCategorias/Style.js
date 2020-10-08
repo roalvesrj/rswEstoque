41b055a33d04635d3e69ce43763f3a41a85276e0
Rômulo Alves <romuloalves.rj@gmail.com>
2020-10-08 17:01:00 -0300

    Correção rota Footer

--- src/components/ScrollViewCategorias/Style.js
+++ src/components/ScrollViewCategorias/Style.js
@@ -1,40 +1,43 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingVertical: 30,
-    paddingHorizontal: 10,
-    backgroundColor: '#fff',
-  },
-  destaque: {
-    paddingBottom: 20,
-    color: '#333',
-    fontSize: 26,
-    fontFamily: 'Euclid Extra Bold',
-  },
-  containerCapas: {
-    flexDirection: 'row',
-    marginBottom: 50,
-  },
-  capa: {
-    width: 185,
-    height: 185,
-    marginBottom: 5,
-  },
-  boxCapa: {
-    marginRight: 15,
-  },
-  destaqueCapas: {
-    marginBottom: 5,
-    color: '#333',
-    fontSize: 16,
-    fontWeight: 'bold',
-    fontFamily: 'Euclid Extra Bold',
-  },
-  textoCapas: {
-    color: '#666',
-  },
+    container: {
+        flex: 1,
+        paddingVertical: 30,
+        paddingHorizontal: 10,
+        backgroundColor: '#fff',
+    },
+    destaque: {
+        paddingBottom: 20,
+        color: '#333',
+        fontSize: 26,
+        fontFamily: 'Euclid Extra Bold',
+    },
+    containerCapas: {
+        flexDirection: 'row',
+        marginBottom: 50,
+    },
+    capa: {
+        width: 185,
+        height: 185,
+        marginBottom: 5,
+        borderWidth: 1,
+        borderColor: '#1c74d8',
+        borderRadius: 5
+    },
+    boxCapa: {
+        marginRight: 15,
+    },
+    destaqueCapas: {
+        marginBottom: 5,
+        color: '#333',
+        fontSize: 16,
+        fontWeight: 'bold',
+        fontFamily: 'Euclid Extra Bold',
+    },
+    textoCapas: {
+        color: '#666',
+    },
 });
 
 export default styles;
